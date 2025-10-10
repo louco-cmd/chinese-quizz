6e3120960c48dfdc61566d35874f172b215d4b74
Louis Cochet <locochet.08@gmail.com>
2025-10-10 10:30:29 +0800

    app mode

---
+++ sw.js
@@ -0,0 +1,11 @@
+// sw.js - version basique
+const CACHE_NAME = 'chinois-app-v1';
+
+self.addEventListener('install', (event) => {
+  console.log('PWA installée !');
+});
+
+self.addEventListener('fetch', (event) => {
+  // Logique de cache simple
+  event.respondWith(fetch(event.request));
+});
